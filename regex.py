33f762e36583934cc22a8a1a6b2f16adc045927a
Flyli0 <kirillrozkov653@gmail.com>
2025-02-19 15:02:37 +0500

    fff

--- regex.py
+++ regex.py
@@ -68,7 +68,7 @@ sss = re.sub(r'([a-z])([A-Z])', r'\1 \2',ss)
 print(sss)
 
 '''''''''''''''''''''''''''''''''Task10'''''''''''''''''''''''''''''''''
-import re
+
 ss = str(input())
 sss = re.sub(r'([a-z])([A-Z])', r'\1_\2',ss)
 print(sss)
